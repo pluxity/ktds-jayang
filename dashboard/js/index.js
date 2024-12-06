1285542bdf8203bd299ade241b1b08317c1ab501
yjsun <yjsun@pluxity.com>
2024-12-06 17:37:02 +0900

    alert,toast,notice,badge,button,tooltip 컨포넌트 작업

---
+++ dashboard/js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function(){
+    // Toast
+    let toast = document.querySelector('.toast');
+    let toastCloseBtn = toast.querySelector('.toast__close');
+    
+    toast.classList.add('toast--active');
+    toastCloseBtn.addEventListener('click', function() {
+        toast.classList.remove('toast--active');
+    });
+});

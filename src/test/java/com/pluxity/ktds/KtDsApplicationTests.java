c25e9fd8668e4d8ac186e784815ffb944826e7bf
Nadongkyu <kaineus86@gmail.com>
2024-12-06 14:49:13 +0900

    Java 21 버전으로 이니셜라이즈

---
+++ src/test/java/com/pluxity/ktds/KtDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluxity.ktds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtDsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

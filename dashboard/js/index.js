67847b582d9d3659398bdfcb01112da878058816
yjsun <yjsun@pluxity.com>
2024-12-13 15:56:09 +0900

    컨포넌트 정리 및 대시보드 배경이미지 지정

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -9,4 +9,30 @@ window.addEventListener('DOMContentLoaded', function(){
     });
 });
 
+/* Header */
+const profileBtn = document.querySelectorAll(".profile .profile__btn");
+profileBtn.forEach(function(btns){ 
+    btns.addEventListener ("click", function() { 
+        btns.classList.toggle('profile__btn--active');
+    });
+})
+
+/* POI Menu */
+function PoiMenuAll() {
+    const PoiAll = document.querySelector(".poi-menu__all .all a");
+    const poiAllPopup = document.querySelector(".poi-menu__all .popup-equip--group");
+
+    PoiAll.addEventListener ("click", function() { 
+        if (poiAllPopup.style.display === "none") {
+            PoiAll.classList.add('active');
+            poiAllPopup.style.display = "block";
+          } else {
+            PoiAll.classList.remove('active');
+            poiAllPopup.style.display = "none";
+          }
+    }); 
+  }
+  PoiMenuAll();
+
+
 
